0c1496007ba47efa34d1f7b256599e014603c4cd
David Scheffler <david.scheffler@student.ibs-ol.de>
2024-08-22 20:29:02 +0200

    added clear() for login and password field after logging in

--- client/src/main/java/de/uol/swp/client/auth/LoginPresenter.java
+++ client/src/main/java/de/uol/swp/client/auth/LoginPresenter.java
@@ -51,6 +51,8 @@ public class LoginPresenter extends AbstractPresenter {
     @FXML
     private void onLoginButtonPressed(ActionEvent event) {
         userService.login(loginField.getText(), passwordField.getText());
+        loginField.clear();
+        passwordField.clear();
     }
 
     /**
