1cf515f785bb1dc25da2e634e703ebe16e6f98d3
moehlenbrockj <jannis.moehlenbrock@cewe.de>
2024-08-26 16:36:07 +0200

    Branch created and packages created. Development now follows.

---
+++ client/src/main/java/de/uol/swp/client/roles/Role.java
@@ -0,0 +1,13 @@
+package de.uol.swp.client.roles;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Role {
+
+    @Getter @Setter
+    private String name;
+
+    @Getter @Setter
+    private String description;
+}
