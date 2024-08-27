b503743cf405d86abf204678b6fd1879b5f7666e
moehlenbrockj <jannis.moehlenbrock@cewe.de>
2024-08-27 20:08:54 +0200

    Update Lombok

--- common/src/main/java/de/uol/swp/common/action/GeneralAction.java
+++ common/src/main/java/de/uol/swp/common/action/GeneralAction.java
@@ -2,35 +2,17 @@ package de.uol.swp.common.action;
 
 import de.uol.swp.common.game.Game;
 import de.uol.swp.common.player.Player;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public abstract class GeneralAction implements Action {
 
     private Player executingPlayer;
     private Game game;
-
-    protected GeneralAction() {
-        //Standard-Konstruktor; kann bei Bedarf erweitert werden
-    }
-
-    @Override
-    public Player getExecutingPlayer() {
-        return this.executingPlayer;
-    }
-
-    @Override
-    public void setExecutingPlayer(Player player) {
-        this.executingPlayer = player;
-    }
-
-    @Override
-    public Game getGame() {
-        return this.game;
-    }
-
-    @Override
-    public void setGame(Game game) {
-        this.game = game;
-    }
 
     @Override
     public abstract boolean isAvailable();
