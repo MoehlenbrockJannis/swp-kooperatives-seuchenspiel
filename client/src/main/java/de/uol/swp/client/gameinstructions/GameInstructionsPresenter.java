e52a97579e3387b59075dc8169294b7e7372d1ca
David Scheffler <david.scheffler@student.ibs-ol.de>
2024-08-28 08:49:44 +0200

    added JavaDoc comments

--- client/src/main/java/de/uol/swp/client/gameinstructions/GameInstructionsPresenter.java
+++ client/src/main/java/de/uol/swp/client/gameinstructions/GameInstructionsPresenter.java
@@ -3,22 +3,42 @@ package de.uol.swp.client.gameinstructions;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 
+/**
+ * The GameInstructionsPresenter class manages the display and hiding of a popup
+ * containing game instructions in the user interface.
+ *
+ * @since 2024-08-27
+ */
 public class GameInstructionsPresenter {
 
     @FXML
     private AnchorPane popupAnchorPane;
 
+    /**
+     * Closes the popup by setting its visibility to false.
+     *
+     * @since 2024-08-27
+     */
     public void closePopup() {
         popupAnchorPane.setVisible(false);
     }
 
+    /**
+     * Opens the popup by setting its visibility to true.
+     *
+     * @since 2024-08-27
+     */
     public void openPopup() {
         popupAnchorPane.setVisible(true);
     }
 
+    /**
+     * Handles the action event triggered when the close button is pressed.
+     *
+     * @param event the ActionEvent triggered by pressing the close button
+     * @since 2024-08-27
+     */
     @FXML
     void onCloseButtonPressed(ActionEvent event) {
         closePopup();
