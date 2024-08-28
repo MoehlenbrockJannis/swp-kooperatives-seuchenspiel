a3f3479ab972c95bf2bdfd24d53ef8126d4fc218
Silas van Thiel <Silas.vanThiel@student.ibs-ol.de>
2024-08-28 10:31:00 +0200

    Updated the ChatServiceTest class with usage of ChatManagement

--- server/src/test/java/de/uol/swp/server/chat/ChatServiceTest.java
+++ server/src/test/java/de/uol/swp/server/chat/ChatServiceTest.java
@@ -5,7 +5,9 @@ import de.uol.swp.common.chat.request.SendChatMessageRequest;
 import de.uol.swp.common.user.User;
 import de.uol.swp.common.user.UserDTO;
 import de.uol.swp.server.EventBusBasedTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.greenrobot.eventbus.EventBus;
 
 import java.time.LocalTime;
 
@@ -16,38 +18,48 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  * Tests the ChatService
  */
 public class ChatServiceTest extends EventBusBasedTest {
-//TODO: Fix ChatServiceTest with Mocking
 
-//    final User defaultUser = new UserDTO("Marco", "test", "marco@test.de");
-//    final ChatService chatService = new ChatService(getBus());
-//
-//    /**
-//     * Tests the onChatRequest method
-//     * @see ChatService
-//     */
-//    @Test
-//    void onChatRequestTest() throws InterruptedException {
-//        SendChatMessageRequest sendChatMessageRequest = new SendChatMessageRequest(defaultUser.getUsername(), "Test", LocalTime.now());
-//        post(sendChatMessageRequest);
-//
-//        assertNotNull(chatService.getChatMessages());
-//        assertEquals(1, chatService.getChatMessages().size());
-//
-//        SendChatMessageRequest sendChatMessageRequest2 = new SendChatMessageRequest(defaultUser.getUsername(), "Test2", LocalTime.now());
-//        post(sendChatMessageRequest);
-//
-//        assertEquals(2, chatService.getChatMessages().size());
-//    }
-//
-//    /**
-//     * Tests the onRetrieveChatRequest method
-//     * @see ChatService
-//     */
-//    @Test
-//    void onRetrieveChatRequestTest() throws InterruptedException {
-//        RetrieveChatRequest retrieveChatRequest = new RetrieveChatRequest();
-//        post(retrieveChatRequest);
-//
-//        assertNotNull(chatService.getChatMessages());
-//    }
+    final User defaultUser = new UserDTO("Marco", "test", "marco@test.de");
+    ChatService chatService;
+    ChatManagement chatManagement;
+
+    /**
+     * Sets up the test environment
+     */
+    @BeforeEach
+    void setUp() {
+        EventBus eventBus = getBus();
+        chatManagement = new ChatManagement();
+        chatService = new ChatService(eventBus, chatManagement);
+    }
+
+    /**
+     * Tests the onChatRequest method
+     * @throws InterruptedException
+     */
+    @Test
+    void onChatRequestTest() throws InterruptedException {
+        SendChatMessageRequest sendChatMessageRequest = new SendChatMessageRequest(defaultUser.getUsername(), "Test", LocalTime.now());
+        post(sendChatMessageRequest);
+
+        assertNotNull(chatManagement.getChatMessages());
+        assertEquals(1, chatManagement.getChatMessages().size());
+
+        SendChatMessageRequest sendChatMessageRequest2 = new SendChatMessageRequest(defaultUser.getUsername(), "Test2", LocalTime.now());
+        post(sendChatMessageRequest2);
+
+        assertEquals(2, chatManagement.getChatMessages().size());
+    }
+
+    /**
+     * Tests the onRetrieveChatRequest method
+     * @throws InterruptedException
+     */
+    @Test
+    void onRetrieveChatRequestTest() throws InterruptedException {
+        RetrieveChatRequest retrieveChatRequest = new RetrieveChatRequest();
+        post(retrieveChatRequest);
+
+        assertNotNull(chatManagement.getChatMessages());
+    }
 }
