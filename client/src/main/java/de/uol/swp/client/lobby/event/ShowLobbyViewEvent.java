2faa43dc096cc2a1b43399fbb60f0b7d71c37ecb
Tom Weelborg <tom.weelborg@student.ibs-ol.de>
2024-08-28 19:33:14 +0200

    Add ShowLobbyViewEvent

--- client/src/main/java/de/uol/swp/client/lobby/event/ShowLobbyViewEvent.java
+++ client/src/main/java/de/uol/swp/client/lobby/event/ShowLobbyViewEvent.java
@@ -1,13 +1,23 @@
 package de.uol.swp.client.lobby.event;
 
+import de.uol.swp.common.lobby.Lobby;
+import de.uol.swp.common.user.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Event used to show the lobby window
+ *
+ * In order to show the lobby create window using this event, post an instance of it
+ * onto the eventBus the SceneManager is subscribed to.
+ *
+ * @author Niklas Wrobel
+ * @see de.uol.swp.client.SceneManager
+ * @since 2024-08-28
+ */
+@Getter
+@RequiredArgsConstructor
 public class ShowLobbyViewEvent {
-    private final String lobbyName;
-
-    public ShowLobbyViewEvent(String lobbyName) {
-        this.lobbyName = lobbyName;
-    }
-
-    public String getLobbyName() {
-        return lobbyName;
-    }
+    private final Lobby lobby;
+    private final User user;
 }
