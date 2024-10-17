6d583a50928b45b6047fc818f4108d958dfbde2f
David Scheffler <david.scheffler@student.ibs-ol.de>
2024-10-17 11:02:26 +0000

    #113: CityMarker auf Fields ändern

--- client/src/main/java/de/uol/swp/client/game/CityMarker.java
+++ client/src/main/java/de/uol/swp/client/game/CityMarker.java
@@ -1,12 +1,12 @@
 package de.uol.swp.client.game;
 
 import de.uol.swp.client.util.ColorService;
-import de.uol.swp.common.map.MapSlot;
+import de.uol.swp.common.map.Field;
+import javafx.animation.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 import lombok.Getter;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 /**
  * JavaFX circle element used to render in GameMapPresenter. This element represents a city on the game map.
@@ -15,49 +15,91 @@ import org.apache.logging.log4j.Logger;
  * @author David Scheffler
  * @since 2024-09-10
  */
-@Getter
 public class CityMarker extends Circle {
 
-    private static final Logger LOG = LogManager.getLogger(CityMarker.class);
+    private FadeTransition fadeTransition;
+    private ScaleTransition scaleTransition;
 
-    private final MapSlot mapSlot;
+    @Getter
+    private final Field field;
 
     /**
      * Constructor
      *
-     * @param mapSlot A mapSlot of the mapType from the current game
+     * @param field A field of the gameMap from the current game
      * @author David Scheffler
      * @since 2024-09-10
      */
-    public CityMarker(MapSlot mapSlot) {
-        this.mapSlot = mapSlot;
-        Color mapSlotColor = ColorService.convertColorToJavaFXColor(mapSlot.getPlague().getColor());
+    public CityMarker(Field field) {
+        this.field = field;
 
-        this.setFill(mapSlotColor);
+        this.setFill(getColor());
         this.setRadius(7);
 
-        this.setOnMouseEntered(event -> this.setOpacity(0.5));
+        initHighlightAnimation();
 
-        this.setOnMouseExited(event -> this.setOpacity(1.0));
-
-        this.setOnMouseClicked(event -> {
-            if (getColor() == mapSlotColor) {
-                this.setFill(Color.WHITE);
-                this.setOpacity(1.0);
-            } else {
-                this.setFill(mapSlotColor);
-            }
-
-            LOG.debug("User clicked on city {} at [{}, {}]", mapSlot.getCity().getName(), mapSlot.getXCoordinate(), mapSlot.getYCoordinate());
-        });
+        this.setOnMouseEntered(event -> this.highlight());
+        this.setOnMouseExited(event -> this.unhighlight());
     }
 
     /**
-     * Returns current fill color of the CityMarker
+     * Initializes the highlight animation.
      *
-     * @return Color of the CityMarker
+     * @since 2024-10-13
      */
-    private Color getColor() {
-        return (Color) this.getFill();
+    private void initHighlightAnimation() {
+        fadeTransition = new FadeTransition(Duration.millis(800), this);
+        fadeTransition.setFromValue(1.0);
+        fadeTransition.setToValue(0.3);
+        fadeTransition.setCycleCount(Animation.INDEFINITE);
+        fadeTransition.setAutoReverse(true);
+
+        scaleTransition = new ScaleTransition(Duration.millis(800), this);
+        scaleTransition.setFromX(1.0);
+        scaleTransition.setToX(1.2);
+        scaleTransition.setFromY(1.0);
+        scaleTransition.setToY(1.2);
+        scaleTransition.setCycleCount(Animation.INDEFINITE);
+        scaleTransition.setAutoReverse(true);
+    }
+
+    /**
+     * Starts the highlight animation.
+     *
+     * @since 2024-10-12
+     */
+    public void highlight() {
+        fadeTransition.play();
+        scaleTransition.play();
+    }
+
+    /**
+     * Stops the highlight animation
+     *
+     * @since 2024-10-12
+     */
+    public void unhighlight() {
+        if (fadeTransition != null) {
+            fadeTransition.stop();
+        }
+        if (scaleTransition != null) {
+            scaleTransition.stop();
+        }
+
+        this.setOpacity(1.0);
+        this.setScaleX(1.0);
+        this.setScaleY(1.0);
+    }
+
+    /**
+     * Returns color of field's plague
+     *
+     * @return Color of field's plague
+     * @see Field
+     * @see de.uol.swp.common.plague.Plague
+     * @since 2024-10-12
+     */
+    public Color getColor() {
+        return ColorService.convertColorToJavaFXColor(field.getPlague().getColor());
     }
 }
