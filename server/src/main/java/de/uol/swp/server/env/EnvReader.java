f33edd0eff234bb213b00d98da95d4418967145b
Dominik Horn <dominik.horn@student.ibs-ol.de>
2025-02-06 07:58:26 +0100

    Add JavaDoc comments to EnvReader interface

--- server/src/main/java/de/uol/swp/server/env/EnvReader.java
+++ server/src/main/java/de/uol/swp/server/env/EnvReader.java
@@ -1,8 +1,23 @@
 package de.uol.swp.server.env;
 
+/**
+ * Interface for reading environment variables
+ */
 public interface EnvReader {
 
+    /**
+     * Reads a string from the environment
+     *
+     * @param key the key of the environment variable
+     * @return the value of the environment variable
+     */
     String readString(String key);
 
+    /**
+     * Reads an integer from the environment
+     *
+     * @param key the key of the environment variable
+     * @return the value of the environment variable
+     */
     int readInt(String key);
 }
