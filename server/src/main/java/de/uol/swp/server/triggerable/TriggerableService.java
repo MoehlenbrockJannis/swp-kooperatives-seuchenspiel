c46e85027a10b516c6a561c351118707321fd667
Dominik Horn <dominik.horn@student.ibs-ol.de>
2025-02-11 13:17:48 +0000

    Bugfix: #168 Mehrere Instanzen von Services im Backend

--- server/src/main/java/de/uol/swp/server/triggerable/TriggerableService.java
+++ server/src/main/java/de/uol/swp/server/triggerable/TriggerableService.java
@@ -1,6 +1,7 @@
 package de.uol.swp.server.triggerable;
 
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
 import de.uol.swp.common.approvable.ApprovableMessageStatus;
 import de.uol.swp.common.approvable.server_message.ApprovableServerMessage;
 import de.uol.swp.common.card.event_card.EventCard;
@@ -22,6 +23,7 @@ import org.greenrobot.eventbus.Subscribe;
 
 import java.util.Optional;
 
+@Singleton
 public class TriggerableService extends AbstractService {
     private final GameManagement gameManagement;
     private final GameService gameService;
