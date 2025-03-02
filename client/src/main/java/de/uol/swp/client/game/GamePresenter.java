61aaf13edea33700f30fbd6fdcf312766f3aeb11
Jannis Möhlenbrock <jannis.moehlenbrock@student.ibs-ol.de>
2025-03-02 12:53:36 +0000

    Bugfix: Der RemainingComponentPresenter zeigt auch nach einem AutoTriggerable die korrekten Daten an

--- client/src/main/java/de/uol/swp/client/game/GamePresenter.java
+++ client/src/main/java/de/uol/swp/client/game/GamePresenter.java
@@ -55,6 +55,9 @@ import lombok.Getter;
 import org.greenrobot.eventbus.Subscribe;
 
 import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -67,6 +70,8 @@ public class GamePresenter extends AbstractPresenter {
     public static final String ANTIDOTE_IMAGE_FOLDER_PATH = "/images/action/AntidoteMarker";
 
     public static final String FILE_EXTENSION_PNG =  ".png";
+
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     @Getter
     private Game game;
@@ -635,8 +640,9 @@ public class GamePresenter extends AbstractPresenter {
             infectionCardsOverviewPresenter.updateLabels();
             infectionCardsOverviewPresenter.updateInfectionMarkerLabel();
             updatePlayerInfo();
-            updateRemainingComponents();
             updateShareKnowledgeActionButton();
+
+            scheduler.schedule(this::updateRemainingComponents, 500, TimeUnit.MILLISECONDS);
         }
     }
 
